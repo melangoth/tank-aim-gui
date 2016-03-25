e692b8a8cf1bea81953824e9fc25e5d09a9e94dc
melangoth <contact@develrage.com>
2016-03-25 20:45:09 +0100

    redefined some drawing

--- tank-aim-gui/src/main/java/MyPanel.java
+++ tank-aim-gui/src/main/java/MyPanel.java
@@ -7,8 +7,13 @@ import java.awt.event.MouseEvent;
  * Created by develrage on 2016. 03. 25..
  */
 class MyPanel extends JPanel {
-    public static final int MONITORLINE1X = 10;
-    public static final int MONITORLINE1Y = 35;
+    // monitor line 1
+    public static final int MLINE_1_LEFT = 10;
+    public static final int MLINE_1_BOTTOM = 20;
+    public static final int MLINE_1_WIDTH = 100;
+    public static final int MLINE_1_HEIGHT = 15;
+
+    // red square
     private int squareX = 50;
     private int squareY = 50;
     private int squareW = 20;
@@ -19,51 +24,52 @@ class MyPanel extends JPanel {
 
         addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent e) {
-                moveSquare(e.getX(),e.getY());
+                moveSquare(e.getX(), e.getY());
             }
         });
 
         addMouseMotionListener(new MouseAdapter() {
             public void mouseDragged(MouseEvent e) {
-                moveSquare(e.getX(),e.getY());
+                moveSquare(e.getX(), e.getY());
             }
         });
     }
 
     private void moveSquare(int x, int y) {
+        System.out.println("Moving square.");
         int OFFSET = 1;
 
         // repaint only if moved
-        if ((squareX!=x) || (squareY!=y)) {
+        if ((squareX != x) || (squareY != y)) {
 
             // repaint monitor area
-            repaint(MONITORLINE1X, MONITORLINE1Y-15, 100, 15);
+            repaint(MLINE_1_LEFT, MLINE_1_BOTTOM - MLINE_1_HEIGHT, MLINE_1_WIDTH, MLINE_1_HEIGHT);
 
             // repaint old position area
-            repaint(squareX,squareY,squareW+OFFSET,squareH+OFFSET);
-            squareX=x;
-            squareY=y;
+            repaint(squareX, squareY, squareW + OFFSET, squareH + OFFSET);
+            squareX = x;
+            squareY = y;
 
             // repaint new position area
-            repaint(squareX,squareY,squareW+OFFSET,squareH+OFFSET);
+            repaint(squareX, squareY, squareW + OFFSET, squareH + OFFSET);
         }
     }
 
     public Dimension getPreferredSize() {
-        return new Dimension(250,200);
+        return new Dimension(250, 200);
     }
 
     public void paintComponent(Graphics g) {
+        System.out.println("Painting component.");
         super.paintComponent(g);
 
         // Draw Text
-        g.drawString("This is my custom Panel!",10,20);
-        g.drawString(String.format("@ %d,%d", squareX, squareY), MONITORLINE1X, MONITORLINE1Y);
+        g.drawString(String.format("@ %d,%d", squareX, squareY), MLINE_1_LEFT, MLINE_1_BOTTOM);
 
         // Draw Rect
         g.setColor(Color.RED);
-        g.fillRect(squareX,squareY,squareW,squareH);
+        g.fillRect(squareX, squareY, squareW, squareH);
         g.setColor(Color.BLACK);
-        g.drawRect(squareX,squareY,squareW,squareH);
+        g.drawRect(squareX, squareY, squareW, squareH);
     }
 }
