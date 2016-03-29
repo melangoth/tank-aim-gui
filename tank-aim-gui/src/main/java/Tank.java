1d0d87c23c5c0ab29f2b57a6338dd571c34a1fcc
melangoth <contact@develrage.com>
2016-03-30 01:30:47 +0200

    Added cannon circle and muzzle point to draw on tanks

--- tank-aim-gui/src/main/java/Tank.java
+++ tank-aim-gui/src/main/java/Tank.java
@@ -3,21 +3,17 @@ import java.awt.*;
 /**
  * Created by develrage on 2016. 03. 25..
  */
-class Tank {
+class Tank extends AnalyserMathTools {
+    private String name;
     private int xPos = 50;
     private int yPos = 50;
     private int width = 15;
     private int height = 15;
     private Color color;
 
-    public Tank(Color color) {
+    public Tank(Color color, String name) {
         this.color = color;
-    }
-
-    public Tank(Color color, int x, int y) {
-        this.color = color;
-        this.xPos = x;
-        this.yPos = y;
+        this.name = name;
     }
 
     public int getCenterX() {
@@ -59,7 +55,27 @@ class Tank {
 
     public void paintSprite(Graphics g) {
         g.setColor(this.color);
-        g.drawRect(xPos, yPos, width, height);
+
+        // main circle with center
+        g.drawOval(xPos, yPos, width, height);
+        int cSize = 4;
+        g.fillOval(getCenterX() - cSize / 2, getCenterY() - cSize / 2, cSize, cSize);
+
+        // cannon circle with muzzle point
+        int tSize = 36;
+        g.drawOval(getCenterX() - tSize / 2, getCenterY() - tSize / 2, tSize, tSize);
+        double[] muzzlePoint = getMuzzlePoint(Analyser.getInstance().getAngle(), tSize / 2);
+        g.fillOval(getCenterX() - cSize / 2 + (int) muzzlePoint[0], getCenterY() - cSize / 2 - (int) muzzlePoint[1], cSize, cSize);
+
+        // position text
         g.drawString(String.format("@ %d,%d", xPos, yPos), xPos, yPos + 20 + height);
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public String getName() {
+        return name;
     }
 }
