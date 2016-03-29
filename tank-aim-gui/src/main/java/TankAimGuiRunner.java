fb56d65867e8b2a01003c234aea5c413be96cda6
melangoth <contact@develrage.com>
2016-03-30 01:31:59 +0200

    todos

--- tank-aim-gui/src/main/java/TankAimGuiRunner.java
+++ tank-aim-gui/src/main/java/TankAimGuiRunner.java
@@ -34,9 +34,7 @@ public class TankAimGuiRunner {
         scr.captureRegion();*/
     }
 
+    // todo make greenTank auto-search, make red-tank manual-search, switch also trajectory
     // todo implement realtime screen capturing
-    // todo separate screener and analizer to dedicated worker thread
-    // todo monitor tanks relative to their last known place
-    // todo later reimplement partial repaint, if performance requires
     // todo Wind: wind=x; power=max; angle= angle +/- (x/2/10)
 }
